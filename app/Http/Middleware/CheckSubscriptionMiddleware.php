446cb746a27cb15519264c88f2c6d6ee300549bb
odejinmisa <odejinmisa@newwavesecosystem.com>
2020-11-19 07:08:21 +0100

    push check subsciption reading fixed

--- app/Http/Middleware/CheckSubscriptionMiddleware.php
+++ app/Http/Middleware/CheckSubscriptionMiddleware.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Carbon\Carbon;
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CheckSubscriptionMiddleware
 {
@@ -17,8 +18,10 @@ class CheckSubscriptionMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
-        if(Carbon::now()->diffInDays(Carbon::parse(\Illuminate\Support\Facades\Auth::user()->subscription), false) < 0){
-            return redirect('/pay');
+        if(Auth::user()->subsciption!='new') {
+            if (Carbon::now()->diffInDays(Carbon::parse(Auth::user()->subscription), false) < 0) {
+                return redirect('/pay');
+            }
         }
         return $next($request);
     }

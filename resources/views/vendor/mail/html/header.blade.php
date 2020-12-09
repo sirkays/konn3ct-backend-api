c6c73b6e324ba2a06d6a52b717a9acbd4ac6452f
odejinmisa <odejinmisa@newwavesecosystem.com>
2020-12-09 07:12:32 +0100

    push publish on mail

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,11 @@
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+@if (trim($slot) === 'Laravel')
+<img src="https://konn3ct.com/assets/images/konn3ctIcon.png" class="logo" alt="Konn3ct Logo">
+@else
+{{ $slot }}
+@endif
+</a>
+</td>
+</tr>

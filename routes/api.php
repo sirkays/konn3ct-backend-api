f1b8b49d9f58b23934c68d7ab80a15b6b2d0a47d
odejinmisa <odejinmisa@newwavesecosystem.com>
2021-01-20 22:12:54 +0100

    fix deploy issue

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DeployController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('deploy', 'DeployController@deploy');
+Route::post('deploy', [DeployController::class, 'deploy']);

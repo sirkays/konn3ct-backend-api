6667b3dd80e25fb4d1e68bebedf7ad211c47e990
odejinmisa <odejinmisa@newwavesecosystem.com>
2021-01-20 22:07:18 +0100

    push changes

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,14 @@
 # activate maintenance mode
 php artisan down
 # update source code
-git pull
+git pull origin dev
 # update PHP dependencies
 composer install --no-interaction --no-dev --prefer-dist
 # --no-interaction Do not ask any interactive question
 # --no-dev  Disables installation of require-dev packages.
 # --prefer-dist  Forces installation from package dist even for dev versions.
 # update database
-php artisan migrate --force
+#php artisan migrate --force
 # --force  Required to run when in production.
 # stop maintenance mode
 php artisan up

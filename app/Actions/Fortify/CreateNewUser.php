e6b3ab9e729c1c3bc2f6030bf39b4cd3a47f8f05
odejinmisa <odejinmisa@newwavesecosystem.com>
2021-01-30 21:55:37 +0100

    push work

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -87,7 +87,9 @@ class CreateNewUser implements CreatesNewUsers
 
             $num = trim(date('siyh'));
             $shuffled = str_shuffle($num);
-            $sfinal = substr($shuffled, 0, 4);
+            $sfin = substr($shuffled, 0, 4);
+            $sfina = substr($input['firstname'], 0, 2);
+            $sfinal = str_shuffle($sfin.$sfina);
             $input['name'] = $input['firstname'] ." Room";
             $input['password_attendee'] = "attendee";
             $input['password_moderator'] = "moderator";

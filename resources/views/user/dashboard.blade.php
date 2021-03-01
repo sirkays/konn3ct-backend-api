0b79f19828993d69e71b76a4c4d0cc02e46fbcf4
odejinmisa <odejinmisa@newwavesecosystem.com>
2021-03-01 11:13:26 +0100

    fix dashboard issue caused by javascript

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -797,16 +797,12 @@
 
 
     <script>
-        import Button from "../../js/Jetstream/Button";
         function copyToClipboard(element) {
             var $temp = $("<input>");
             $("body").append($temp);
             $temp.val($(element).text()).select();
             document.execCommand("copy");
             $temp.remove();
-        }
-        export default {
-            components: {Button}
         }
     </script>
 

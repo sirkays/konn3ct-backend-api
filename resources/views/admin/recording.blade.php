e34ccfc589e2fd037a721bf8f75163d71eb5bd0a
odejinmisa <odejinmisa@newwavesecosystem.com>
2021-04-01 05:40:05 +0100

    comment out size on admin recording

--- resources/views/admin/recording.blade.php
+++ resources/views/admin/recording.blade.php
@@ -67,9 +67,9 @@
                                             <span class="text-dark font-weight-600 d-block font-size-10">
 													{{$record['playback']['format']['length']}} Minutes
 												</span>
-                                            <span class="text-dark font-weight-600 d-block font-size-10">
-													{{ number_format(($record['size']/1000000))."MB"}}
-												</span>
+{{--                                            <span class="text-dark font-weight-600 d-block font-size-10">--}}
+{{--													{{ number_format(($record['size']/1000000))."MB"}}--}}
+{{--												</span>--}}
                                         </td>
 
                                         <td>
@@ -179,9 +179,9 @@
                                             <span class="text-dark font-weight-600 d-block font-size-16">
 													{{$record['playback']['format']['length']}} Minutes
 												</span>
-                                            <span class="text-dark font-weight-600 d-block font-size-16">
-													{{ number_format(($record['size']/1000000))."MB"}}
-												</span>
+{{--                                            <span class="text-dark font-weight-600 d-block font-size-16">--}}
+{{--													{{ number_format(($record['size']/1000000))."MB"}}--}}
+{{--												</span>--}}
                                         </td>
                                         <td style="overflow-wrap: break-word; min-width: 50px; max-width: 150px;">
                                             <span

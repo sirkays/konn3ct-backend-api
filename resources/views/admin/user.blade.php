4feec25cdb6b64512f6c83e2cc99cad4c467634e
odejinmisa <odejinmisa@newwavesecosystem.com>
2021-05-01 20:48:04 +0100

    fix crash on manage user

--- resources/views/admin/user.blade.php
+++ resources/views/admin/user.blade.php
@@ -446,10 +446,10 @@
                                                                     class="text-dark font-weight-600 d-block font-size-16">
 													{{$record['playback']['format']['length']}} Minutes
 												</span>
-                                                                <span
-                                                                    class="text-dark font-weight-600 d-block font-size-16">
-													{{ number_format(($record['size']/1000000))."MB"}}
-												</span>
+{{--                                                                <span--}}
+{{--                                                                    class="text-dark font-weight-600 d-block font-size-16">--}}
+{{--													{{ number_format(($record['size']/1000000))."MB"}}--}}
+{{--												</span>--}}
                                                             </td>
                                                             <td style="min-width: 50px; max-width: 150px">
                                                                 <span

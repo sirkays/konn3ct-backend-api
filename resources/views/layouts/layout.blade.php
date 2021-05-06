f3974608aa54680243186b85afdb8da95028bc36
odejinmisa <odejinmisa@newwavesecosystem.com>
2021-05-06 09:44:19 +0100

    push changes

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -618,6 +618,31 @@
     }
 </script>
 
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/8.5.0/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/8.5.0/firebase-analytics.js"></script>
+
+<script>
+    // Your web app's Firebase configuration
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    var firebaseConfig = {
+        apiKey: "AIzaSyCzgGlhWfz-tS0zJibnaXG6AN4RhiNdHvM",
+        authDomain: "mega-free-points.firebaseapp.com",
+        databaseURL: "https://mega-free-points.firebaseio.com",
+        projectId: "mega-free-points",
+        storageBucket: "mega-free-points.appspot.com",
+        messagingSenderId: "64273173216",
+        appId: "1:64273173216:web:4b4846c110feb97d6ede85",
+        measurementId: "G-MG1YVR4NQ9"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+</script>
+
 </body>
 
 </html>

d5df475a8202e7a8915d1c59a7c9d784d60bc51f
odejinmisa <odejinmisa@newwavesecosystem.com>
2021-05-06 12:53:48 +0100

    google anal

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -317,6 +317,17 @@
 {{--        drift.load('9u4f4f3mumcc');--}}
 {{--    </script>--}}
 <!-- End of Async Drift Code -->
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-196433825-2"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-196433825-2');
+    </script>
+
 </head>
 <body>
 <!-- header -->
@@ -617,32 +628,6 @@
         el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
     }
 </script>
-
-<!-- The core Firebase JS SDK is always required and must be listed first -->
-<script src="https://www.gstatic.com/firebasejs/8.5.0/firebase-app.js"></script>
-
-<!-- TODO: Add SDKs for Firebase products that you want to use
-     https://firebase.google.com/docs/web/setup#available-libraries -->
-<script src="https://www.gstatic.com/firebasejs/8.5.0/firebase-analytics.js"></script>
-
-<script>
-    // Your web app's Firebase configuration
-    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-    var firebaseConfig = {
-        apiKey: "AIzaSyCzgGlhWfz-tS0zJibnaXG6AN4RhiNdHvM",
-        authDomain: "mega-free-points.firebaseapp.com",
-        databaseURL: "https://mega-free-points.firebaseio.com",
-        projectId: "mega-free-points",
-        storageBucket: "mega-free-points.appspot.com",
-        messagingSenderId: "64273173216",
-        appId: "1:64273173216:web:4b4846c110feb97d6ede85",
-        measurementId: "G-MG1YVR4NQ9"
-    };
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
-    firebase.analytics();
-</script>
-
 </body>
 
 </html>

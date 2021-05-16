92a5f23afa77085f18e40452af9dd8c6e46768fc
odejinmisa <odejinmisa@newwavesecosystem.com>
2021-05-16 17:20:19 +0100

    push chnages

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -72,7 +72,7 @@ class RoomController extends Controller
 
         $r=RoomModel::create($input);
 
-        $createMeeting = Bigbluebutton::initCreateMeeting([
+        $createMeeting = \Bigbluebutton::initCreateMeeting([
             'meetingID' => $r->id,
             'meetingName' => $input['name'],
             'attendeePW' => $input['password_attendee'],
@@ -146,7 +146,7 @@ class RoomController extends Controller
 //$meetingParams->setLockSettingsLockedLayout
 //$meetingParams->setLockSettingsLockOnJoin
 //    $meetingParams->setFreeJoin
-        $bbb = Bigbluebutton::create($createMeeting);
+        $bbb = \Bigbluebutton::create($createMeeting);
 //       $bbb='{"returncode":"SUCCESS","internalMeetingID":"b1d5781111d84f7b3fe45a0852e59758cd7a87e5-1602475017235","parentMeetingID":"bbb-none","createTime":"1602475017235","voiceBridge":"09857","dialNumber":"613-555-1234","createDate":"Mon Oct 12 03:56:57 UTC 2020","hasUserJoined":"false","duration":"100","hasBeenForciblyEnded":"false","messageKey":[],"message":[]}';
 
         $bba=json_decode($bbb, true);
@@ -207,11 +207,11 @@ class RoomController extends Controller
                 ->with('error', 'Invalid Room!');
         }
 
-        $ms = Bigbluebutton::isMeetingRunning($i->id);
+        $ms = \Bigbluebutton::isMeetingRunning($i->id);
 
         if($ms==1) {
             return redirect()->to(
-                Bigbluebutton::join([
+                \Bigbluebutton::join([
                     'meetingID' => $i->id,
                     'userName' => Auth::user()->lastname . " " . Auth::user()->firstname,
                     'password' => $i->password_moderator //which user role want to join set password here
@@ -284,7 +284,7 @@ class RoomController extends Controller
             $mdata['identifier']=$i->id.rand();
             MeetingsModel::create($mdata);
 
-            $url = Bigbluebutton::start([
+            $url = \Bigbluebutton::start([
                 'meetingID' => $i->id,
                 'moderatorPW' => $i->password_moderator, //moderator password set here
                 'attendeePW' => $up, //attendee password here
@@ -335,7 +335,7 @@ class RoomController extends Controller
 
         $u=User::find($i->user_id);
 
-        $ms = Bigbluebutton::isMeetingRunning($i->id);
+        $ms = \Bigbluebutton::isMeetingRunning($i->id);
 //        $ms=1;
 
         $mdata['meeting_id']=$i->id;
@@ -359,7 +359,7 @@ class RoomController extends Controller
 //            return back()
 //                ->with('error', 'Meeting has not started!');
         }else{
-            $mds = Bigbluebutton::getMeetingInfo([
+            $mds = \Bigbluebutton::getMeetingInfo([
                 'meetingID' => $i->id,
                 'moderatorPW' => $i->password_moderator //moderator password set here
             ]);
@@ -415,7 +415,7 @@ class RoomController extends Controller
             $password_attendee="moderator";
         }
 
-        $ms = Bigbluebutton::isMeetingRunning($i->id);
+        $ms = \Bigbluebutton::isMeetingRunning($i->id);
 
         $mdata['meeting_id']=$i->id;
         $mdata['name']=$name;
@@ -442,7 +442,7 @@ class RoomController extends Controller
         MeetingsModel::create($mdata);
 
         return redirect()->to(
-            Bigbluebutton::join([
+            \Bigbluebutton::join([
                 'meetingID' => $i->id,
                 'userName' => $name,
                 'password' => $password_attendee, //which user role want to join set password here
@@ -555,7 +555,7 @@ class RoomController extends Controller
 
     public function roomstatus($url){
         $i = RoomModel::where('url', $url)->first();
-        $ms = Bigbluebutton::isMeetingRunning($i->id);
+        $ms = \Bigbluebutton::isMeetingRunning($i->id);
 //        $ms=0;
         if($ms!=1){
             return response()->json(['status'=>0, 'message'=>'Meeting not started']);

1b8a66bd6d6c9c9486c2c6945ba95486f8c46969
odejinmisa <odejinmisa@newwavesecosystem.com>
2021-08-18 08:29:03 +0100

    new version

--- resources/views/join_session.blade.php
+++ resources/views/join_session.blade.php
@@ -24,7 +24,7 @@
                         <div class="px-3 py-2 col-12 mr-2">
                             <div class="input-group mb-3">
                                 <input type="text" class="form-control" name="url"
-                                       value="{{$url ?? old('url')}}"
+                                       value="{{$url ?? session(['url'])}}"
                                        placeholder="Paste invite link or Enter meeting room name"
                                        aria-label="First Name" aria-describedby="basic-addon1" required>
                             </div>
@@ -36,7 +36,7 @@
                     <div class="row">
                         <div class="px-3 py-2 col-12 mr-2">
                             <div class="input-group mb-3">
-                                <input type="text" class="form-control" name="name" value="{{old('name')}}"
+                                <input type="text" class="form-control" name="name" value="{{session(['name'])}}"
                                        placeholder="Enter your name e.g. Samji Diamond" aria-label="First Name"
                                        aria-describedby="basic-addon1" required>
                             </div>

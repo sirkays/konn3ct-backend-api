3b72d989279256e33d1fc5974f0bbe7da7f56001
odejinmisa <odejinmisa@newwavesecosystem.com>
2022-11-07 17:07:32 +0100

    push changes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\Api\App\AuthController;
 use App\Http\Controllers\Api\App\ChatController;
 use App\Http\Controllers\Api\RoomController;
 use App\Http\Controllers\Api\UserController;
@@ -65,27 +64,6 @@ Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::get('start-a-room/{id}', [RoomController::class, 'startaRoom']);
 });
 
-Route::group(['prefix' => 'app'], function () {
-    Route::post('login', [AuthController::class, 'login']);
-    Route::post('register', [AuthController::class, 'register']);
-    Route::post('forgot-password-request', [AuthController::class, 'reset_password_request']);
-    Route::post('forgot-password', [AuthController::class, 'reset_password_submit']);
-    Route::post('verify-code', [AuthController::class, 'verifyCode']);
-    Route::post('validate-meeting', [AuthController::class, 'validateMeeting']);
-    Route::post('join-room', [RoomController::class, 'joinAppRoom']);
-
-    Route::group(['middleware' => 'auth:sanctum'], function () {
-        Route::get('chats', [ChatController::class, 'fetchChats']);
-        Route::get('chats/participants/{id}', [ChatController::class, 'fetchParticipants']);
-        Route::get('chats/messages/{id}', [ChatController::class, 'fetchMessages']);
-        Route::delete('chats/message/delete/{id}', [ChatController::class, 'deleteMessage']);
-        Route::post('chat', [ChatController::class, 'sendMessage']);
-        Route::post('chat/enroll', [ChatController::class, 'enrol2Chat']);
-        Route::post('chat/unenroll', [ChatController::class, 'unenrol2Chat']);
-        Route::post('validate-user', [ChatController::class, 'validateUser']);
-        Route::post('validate-phones', [ChatController::class, 'validatePhones']);
-    });
-});
-
 
 require __DIR__ . '/api_integration.php';
+require __DIR__ . '/app.php';

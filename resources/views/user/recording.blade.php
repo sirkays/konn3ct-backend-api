c7c01ae4c2e3ac0eba9f335a305108008b87a1f7
odejinmisa <odejinmisa@newwavesecosystem.com>
2023-05-22 08:34:38 +0100

    fixing recording error

--- resources/views/user/recording.blade.php
+++ resources/views/user/recording.blade.php
@@ -58,9 +58,9 @@
                                                 <span class="text-dark font-weight-600 d-block font-size-10">
 													{{$record['participants']}} Participants
 												</span>
-                                            <span class="text-dark font-weight-600 d-block font-size-10">
-													{{$record['playback']['format']['length']}} Minutes
-												</span>
+                                            {{--                                            <span class="text-dark font-weight-600 d-block font-size-10">--}}
+                                            {{--													{{$record['playback']['format']['length']}} Minutes--}}
+                                            {{--												</span>--}}
 
                                             <span class="text-dark font-weight-600 d-block font-size-10">
 													{{ number_format((($record['playback']['format']['size'] ?? 1)/1000000))."MB"}}

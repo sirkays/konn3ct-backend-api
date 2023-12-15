38cd04fb4ff8b4d038c76bdc70658813d9726e4f
Odejinmi Samuel <odejinmisamuel@gmail.com>
2023-12-15 06:01:44 +0000

    commit

--- routes/api.php
+++ routes/api.php
@@ -66,7 +66,7 @@ Route::post('check-room', [RoomController::class, 'checkRoom']);
 Route::post('hook/meeting', [\App\Http\Controllers\WebhookController::class, 'meeting']);
 
 
-Route::apiResource('k4/donation', [\App\Http\Controllers\Api\DonationController::class, 'store']);
+Route::apiResource('k4/donation', \App\Http\Controllers\Api\DonationController::class);
 
 
 Route::group(['middleware' => 'resellerAuth', 'prefix' => 'reseller'], function () {

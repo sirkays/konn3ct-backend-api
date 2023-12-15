8dcbea0465beaf86afde5bfce26b0c00a96939c6
Odejinmi Samuel <odejinmisamuel@gmail.com>
2023-12-15 14:43:43 +0000

    commit

--- app/Http/Controllers/Api/DonationController.php
+++ app/Http/Controllers/Api/DonationController.php
@@ -55,7 +55,7 @@ class DonationController extends Controller
             'amount' => $input['amount']
         ]);
 
-        return response()->json(['success' => true, 'message' => 'Donation created successfully', 'data'=>$donation]);
+        return response()->json(['success' => true, 'message' => 'Donation created successfully', 'data'=>$donation->id]);
 
     }
 
